e899b4121be04c439b081f0b4cf3fbbd468b7368
Ryan Farquharson <ryan.farquharson@csiro.au>
2018-11-29 15:34:39 +1030

    Added the 201516 census data in.

--- scripts/combine_epochs.R
+++ scripts/combine_epochs.R
@@ -4,13 +4,22 @@ library(readr)
 library(dplyr)
 
 commodities_200506 <- read_csv("./data/commodities_200506_SA2.csv")
+commodities_200506$Commodity_code <- as.character(commodities_200506$Commodity_code)
 
-commodities_201011 <- read_csv("./data/commodities_201011_SA2.csv", col_types = "")
+commodities_201011 <- read_csv("./data/commodities_201011_SA2.csv")
+commodities_201011$Commodity_code <- as.character(commodities_201011$Commodity_code)
 
-# getting a warning message about parsing failures due to column types
+commodities_201516 <- read_csv("./data/commodities_201516_ASGS.csv")
 
-colnames(commodities_200506)
 
-colnames(commodities_201011)
+# colnames(commodities_200506)
+# colnames(commodities_201011)
+# colnames(commodities_201516)
 
-commodities_epochs <- bind_rows(list('200506' = commodities_200506, '201011' = commodities_201011), .id = "epoch")
+# combine census years for potential use in defining species and magement over epochs.
+
+commodities_epochs <- bind_rows(list('200506' = commodities_200506, '201011' = commodities_201011, '201516' = commodities_201516),
+                                .id = "census")
+
+write_csv(commodities_epochs, "./data/commodities_epochs.csv")
+
