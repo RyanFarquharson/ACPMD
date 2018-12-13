fe496d7a72eed3e6afc39ba1c01197c5888dfc2a
Ryan Farquharson <ryan.farquharson@csiro.au>
2018-12-13 18:50:12 +1030

    Wrote code to loop through workbook and create tidy data from each worksheet

--- scripts/data_import.R
+++ scripts/data_import.R
@@ -25,6 +25,9 @@ names_row <- 5
 skip_n <- names_row -1
 remove_last_rows <- -3
 
+
+# snippet code
+
 number_of_cols <- ncol(head((read_excel(path, sheet = sheet_n, skip = skip_n)), n = 1))
 
 cola <- head((read_excel(path, sheet = sheet_n, skip = skip_n, col_names = FALSE)), n = 1) %>% 
@@ -50,7 +53,33 @@ table_name_Number_of_establishmments <- select(table_name, "Area", ends_with("es
 
 # iterate through sheets
 
-for (i in seq_along(sheets[-1])) {
+for (i in 2:length(sheets)) {
   sheet_n <- i
   
+  number_of_cols <- ncol(head((read_excel(path, sheet = sheet_n, skip = skip_n)), n = 1))
+  
+  cola <- head((read_excel(path, sheet = sheet_n, skip = skip_n, col_names = FALSE)), n = 1) %>% 
+    select(seq(2, number_of_cols, 2)) %>%
+    rep(each = 2)
+  
+  # use paste to put alternating ' _Estimate' or ' _Number of establishments' next to each column name.
+  
+  colnames <- c("Area", paste(cola, rep(c(" _Estimate", " _Number of establishments"), (number_of_cols - 1) / 2)))
+  
+  # read in data and put in column names
+  
+  table_name <- head((read_excel(path, sheet = sheet_n, skip = names_row + 1, col_names = colnames)), n = remove_last_rows)
+  
+  # create tidy tables for Estimates and Number of establishments
+  
+  table_name_Estimate <- paste("NSW_Table",i,"Estimate", sep="_")
+  
+  table_name_Number_of_establishmments <- paste("NSW_Table",i,"Number_of_establishmments", sep="_")
+  
+  assign(table_name_Estimate, select(table_name, "Area", ends_with("Estimate")) %>%
+    gather(Commodity, Estimate, -Area, na.rm = TRUE))
+  
+  assign(table_name_Number_of_establishmments, select(table_name, "Area", ends_with("establishments")) %>%
+    gather(Commodity, Number_of_establishments, -Area, na.rm = TRUE))
 }
+
